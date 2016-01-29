72de3983c3d2113d93ce3d2f2dc20ded82bf9ef2
subsuperhuman <joel@barbarianquest.com>
2016-01-29 23:27:54 +0000

    added a bunch of basic mechanics and some new scripts

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+	public float offset;			// The offset at which the Health Bar follows the player.
+	
+	private Transform player;		// Reference to the player.
+
+	public float followSpeed;
+
+
+	void Awake ()
+	{
+		// Setting up the reference.
+		player = GameObject.Find("Player1").transform;
+	}
+
+	void Update ()
+	{
+		//rotate to look at the player
+		//transform.LookAt(player.position);
+		//transform.Rotate(new Vector3(0,-90,0),Space.Self);//correcting the original rotation
+		
+		
+		//move towards the player
+		if (Vector3.Distance(transform.position,player.position)>offset){//move if distance from target is greater than 1
+			float direction;
+
+			if((transform.position.x - player.position.x) > 0)
+			{
+				direction = -1;
+			}
+			else
+			{
+				direction = 1;
+			}
+
+			transform.Translate(new Vector3(direction*followSpeed* Time.deltaTime,0,0) );
+		}
+	}
+}
---
+++ Assets/Scripts/TrackPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPlayer : MonoBehaviour {
+
+	public Vector3 offset;			// The offset at which the Health Bar follows the player.
+	
+	private Transform player;		// Reference to the player.
+
+	// Use this for initialization
+	void Start () {
+		// Setting up the reference.
+		player = GameObject.Find("Main Camera").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 position = transform.position;
+
+		Vector3 newPos = position;
+
+		newPos.x = player.position.x;
+
+		transform.position = newPos;
+
+	}
+}
