858cfa640162f2bfbcd954082b9c29745a8c1ad7
subsuperhuman <joel@barbarianquest.com>
2016-01-30 14:24:05 +0000

    added in a janky broken sprite for the shaman

--- Assets/Scripts/SeasonController.cs
+++ Assets/Scripts/SeasonController.cs
@@ -5,6 +5,10 @@ public class SeasonController : MonoBehaviour {
 
 	public string season = "spring";
 
+	private bool seasonChange = false;
+	private bool toggleSeason = false;
+
+
 	// Use this for initialization
 	void Start () {
 
@@ -15,9 +19,35 @@ public class SeasonController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown (KeyCode.Q)) {
-			season = "summer";
+		if (!Input.GetKeyDown (KeyCode.Q)) {
+			toggleSeason = false;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Q) && !toggleSeason) {
+			toggleSeason = true;
+			seasonChange = true;
+		}
+
+		if (seasonChange) {
+			changeSeason();
 		}
 		
+	}
+
+	void changeSeason() {
+
+		if (season == "spring") {
+			season = "summer";
+		} else if (season == "summer") {
+			season = "autumn";
+		} else if (season == "autumn") {
+			season = "winter";
+		} else if (season == "winter") {
+			season = "spring";
+		}
+
+		print (season);
+
+		seasonChange = false;
 	}
 }
