a6d25984c0bca15605d777733800129c8782c5a7
subsuperhuman <joel@barbarianquest.com>
2016-01-30 00:12:55 +0000

    added a water level script

---
+++ Assets/Scripts/WaterBehaviours.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterBehaviours : MonoBehaviour {
+
+	public float defaultLevel;
+
+	private float level = 0;
+
+	public Camera cam; 
+
+	public float baseLevel;
+
+	// Use this for initialization
+	void Start () {;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		level = level;
+
+		Vector3 newP;
+
+
+
+		newP.x = cam.transform.position.x;
+		newP.y = defaultLevel + level + baseLevel;
+		newP.z = transform.position.z;
+
+		transform.position = newP;
+	
+	}
+}
