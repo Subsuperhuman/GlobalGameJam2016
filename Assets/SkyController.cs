fa1a2439d613233a52eeb08596649ea2cccc0d96
subsuperhuman <joel@barbarianquest.com>
2016-01-31 13:50:57 +0000

    walk in the rain

---
+++ Assets/SkyController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyController : MonoBehaviour {
+
+	public Material springMat;
+	public Material summerMat;
+	public Material autumnMat;
+	public Material winterMat;
+
+	private SeasonController seasonCtrl;
+	
+	private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+		seasonCtrl = (SeasonController) FindObjectOfType (typeof(SeasonController));
+		meshRenderer = GetComponent<MeshRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (seasonCtrl.season == "spring") {
+			meshRenderer.material = springMat;
+		}
+		if (seasonCtrl.season == "summer") {
+			meshRenderer.material = summerMat;
+		}
+		if (seasonCtrl.season == "autumn") {
+			meshRenderer.material = autumnMat;
+		}
+		if (seasonCtrl.season == "winter") {
+			meshRenderer.material = winterMat;
+		}
+	
+	}
+}
