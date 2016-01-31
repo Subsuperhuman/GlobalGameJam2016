2fd3f288195733f0a0e49840505c5bc102c74299
subsuperhuman <joel@barbarianquest.com>
2016-01-31 06:40:32 +0000

    audioaudio

---
+++ Assets/AmbienceController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbienceController : MonoBehaviour {
+
+	AudioSource source;
+
+	AudioClip clip;
+
+	public AudioClip springAmbience;
+	public AudioClip summerAmbience;
+	public AudioClip autumnAmbience;
+	public AudioClip winterAmbience;
+
+	private SeasonController seasonCtrl;
+
+	// Use this for initialization
+	void Start () {
+		seasonCtrl = (SeasonController) FindObjectOfType (typeof(SeasonController));
+		source = GetComponent<AudioSource> ();
+		clip = GetComponent<AudioClip> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (seasonCtrl.season == "spring") {
+			source.clip = springAmbience;
+		}
+		if (seasonCtrl.season == "summer") {
+			source.clip = summerAmbience;
+		}
+		if (seasonCtrl.season == "autumn") {
+			source.clip = autumnAmbience;
+		}
+		if (seasonCtrl.season == "winter") {
+			source.clip = winterAmbience;
+		}
+	}
+}
